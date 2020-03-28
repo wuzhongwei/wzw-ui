eee073e93df5649a01ec1c713a6ad43bce64bdd6
吴忠威 <wuzhongwei@wuzhongweideMacBook-Pro.local>
2020-03-29 00:23:29 +0800

    优化

--- examples/main.js
+++ examples/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 // import {Button} from '@/index'
-import {Button, Input} from '../lib/index.js'
+import Wzw, {Button, Input} from '../lib/index.js'
 import '../lib/style/index.css'
 
 Vue.config.productionTip = false
-console.log('Wzw', Button)
+console.log('Wzw', Button, Wzw)
 Vue.use(Button)
 Vue.use(Input)
 
